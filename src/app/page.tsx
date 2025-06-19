06d884fbda4520422bb8f7d2235ccd6a4c855041
rupamjit <rupamjitghosh51@gmail.com>
2025-06-19 12:14:47 +0530

    setup sidebar using scadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import React from "react";
+import { AppSidebarClient } from "./_AppSidebarClient";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+      <Sidebar collapsible="icon" className="overflow-hidden">
+        <SidebarHeader className="flex-row">
+          <SidebarTrigger />
+          <span className="text-xl text-nowrap">HireSpot</span>
+        </SidebarHeader>
+        <SidebarContent></SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>nbcwjebkj</SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+      <main className="flex-1">cjnvkjwne</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
+};
 
-export default HomePage
+export default HomePage;
